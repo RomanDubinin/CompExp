b523634a3722a2d4d8b23f2c41f9a8d0c97df3a7
RomanDubinin <roman230394@yandex.ru>
2016-05-06 11:09:32 +0500

    прогонка

--- lab_2_2.py
+++ lab_2_2.py
@@ -11,15 +11,14 @@ l = 2 + 0.1 * N
 def F(x, y, d_y):
     return y + l*x*(1-x) + 2*l + 2
 
+
 def G(x, y, d_y):
     return y
 
-def real_func2(x):
-    return (1/2)*e**(-x-1) * (2*e**(x+1) *(l*(x*x - x + 4) - 2) - 2*(l-1)*e**(2*x + 1) + e*(2-6*l) - e**(2*x) + e**(2*x + 2) - e**2 + 1)
-
 
 def real_func(x):
-    return 2.4*x*x - 2.4*x + 0.999999*e**x + e**(-x) - 2
+    return -2 + math.exp(-x) + math.exp(x) + l*(-1+x)*x
+
 
 def real_func_method(x_points):
     y_points = []
@@ -27,8 +26,6 @@ def real_func_method(x_points):
         y_points.append(real_func(x_points[i]))
 
     return y_points
-
-
 
 
 def explicit_eiler(f, x_points, y_0, z_0, h):
@@ -43,6 +40,7 @@ def explicit_eiler(f, x_points, y_0, z_0, h):
 
     return y_points
 
+
 def recount_eiler(f, x_points, y_0, z_0, h):
     y_points = [y_0]
     z_points = [z_0]
@@ -55,18 +53,6 @@ def recount_eiler(f, x_points, y_0, z_0, h):
 
     return y_points
 
-
-def recount_eiler_MY(f, x_points, y_0, z_0, h):
-    y_points = [y_0]
-    z_points = [z_0]
-
-    for i in range(len(x_points) - 1):
-        new_y = y_points[i] + (h/2) * (z_points[i] + y_points[i] + h*z_points[i])
-        new_z = z_points[i] + (h/2) * (f(x_points[i], y_points[i], z_points[i]) + x_points[i] + h*f(x_points[i], y_points[i], z_points[i]))
-        y_points.append(new_y)
-        z_points.append(new_z)
-
-    return y_points
 
 def runge_kutta(f, x_points, y_0, z_0, h):
     y_points = [y_0]
@@ -96,12 +82,23 @@ def get_next_M(x_points, F, method, y_0, dy_n, last_M, h):
 
     return last_M - fi/d_fi
 
+
+def progon(x_points, dy_n, h):
+    lamda = [0]
+    mu = [0]
+    last_number = len(x_points) - 1
+    for i in range(len(x_points)):
+        lamda.append(-1/(lamda[i] - 2 -h*h))
+        mu.append((h*h*(l*x_points[i]*(1-x_points[i]) + 2*l + 2) - mu[i]) / (lamda[i] - 2 - h*h))
+
+    y_points = [(h*dy_n + mu[last_number]) / (1 - lamda[last_number])]*len(x_points)
+    for i in range(last_number, 0, -1):
+        y_points[i-1] = (lamda[i-1]*y_points[i] + mu[i-1])
+
+    return y_points
+
 n = int(sys.argv[1])
 e = 2.71828
-
-
-
-
 
 x_points = np.linspace(0, 1, n+1, endpoint=True)
 y_0 = 0
@@ -113,7 +110,17 @@ methods = [
     {"f": recount_eiler,  "name": "Эйлер с пересчётом"},
     {"f": runge_kutta,    "name": "Рунге-Кутта"}
 ]
-colors = ["blue", "red", "purple", "orange", "green"]
+
+p = figure(title="", plot_width=1100, plot_height=600)
+y_points = real_func_method(x_points)
+p.line(x=x_points, y=y_points, color="black", legend="real", line_width=2)
+
+y_points = progon(x_points, right_boundary, h)
+p.line(x=x_points, y=y_points, color="red", legend="progon", line_width=2)
+output_file("progon_{0}_points.html".format(n))
+p.legend.orientation = "top_left"
+show(p)
+
 for i in range(len(methods)):
     p = figure(title="", plot_width=1100, plot_height=600)
 
@@ -133,8 +140,9 @@ for i in range(len(methods)):
         p.line(x=x_points, y=y_points[j], color="red", legend=methods[i]["name"])
     p.circle(x=x_points, y=y_points[-1], color="red", legend=methods[i]["name"])
 
-    output_file("{0}_points.html".format(i))
+    output_file("{0}_{1}_points.html".format(methods[i]["name"], n))
     p.legend.orientation = "top_left"
     show(p)
+
 
 
